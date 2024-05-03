c788384f5a8a84887d439f0a7a2d28d9b5c0d331
Sam007619 <sujithsam098@gmail.com>
2024-05-03 16:02:04 +0530

    update

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
+  chromeWebSecurity:false,
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
